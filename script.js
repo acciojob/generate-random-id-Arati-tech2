0bb35ca5e31860e8bcc483859e767c9a210b3682
Arati-tech2 <aratiwak2@gmail.com>
2026-03-19 15:23:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function makeid(l) {
   // write your code here
+	var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var res = "";
+	for(let i = 0; i<l;i++){
+		res += 
+			Math.floor(
+					char_list.random() * char_list.length;)
+	}
+	
 }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
